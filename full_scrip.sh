ff6ca7f10372ed1f45bc479906d38c05ca07133b
DongLieu <duongduydongtq@gmail.com>
2023-05-30 00:51:40 +0700

    scrip auto push

--- full_scrip.sh
+++ full_scrip.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 for i in {1..3}
 do
-    echo "o" >> filechange.txt
+    for i in {1..1000}
+    do
+        echo "o" >> filechange.txt
+    done
     git add .
     git commit -m"scrip auto push"
     git push origin main
